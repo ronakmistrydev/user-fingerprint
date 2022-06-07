3548dd889beeb02f6d10f197b3c017e143ea43be
Ronak Mistry <ronakmistry36@gmail.com>
2022-06-07 17:34:01 -0500

    Expr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,8 @@ import { lookup } from 'geoip-lite';
 export class AppController {
   @Get()
   appHandler(@Req() request: Request): Record<string, string | number | any> {
+    console.log('Remote address', request.socket.remoteAddress);
+    console.log('Local address', request.socket.localAddress);
     const ipAddress = request.ip.toString().replace('::ffff:', '').trim();
 
     const enhancedData = lookup(ipAddress);
@@ -13,7 +15,14 @@ export class AppController {
     if (enhancedData) {
       return {
         ipAddress,
-        ...enhancedData,
+        country: enhancedData.country,
+        region: enhancedData.region,
+        city: enhancedData.city,
+        timezone: enhancedData.timezone,
+        location: {
+          lat: enhancedData.ll[0],
+          lng: enhancedData.ll[1],
+        },
       };
     }
 
