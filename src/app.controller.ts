7e13d7e35609f2e511f6cfbad116b96b9f82619b
Ronak Mistry <ronakmistry36@gmail.com>
2022-06-07 17:23:15 -0500

    IP lookup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,24 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
+import geoIp from 'geoip-lite';
 
 @Controller()
 export class AppController {
   @Get()
-  appHandler(@Req() request: Request): string {
-    return request.ip.toString().replace('::ffff:', '').trim();
+  appHandler(@Req() request: Request): Record<string, string | number | any> {
+    const ipAddress = request.ip.toString().replace('::ffff:', '').trim();
+
+    const lookup = geoIp.lookup(ipAddress);
+
+    if (lookup) {
+      return {
+        ipAddress,
+        ...lookup,
+      };
+    }
+
+    return {
+      ipAddress,
+    };
   }
 }
