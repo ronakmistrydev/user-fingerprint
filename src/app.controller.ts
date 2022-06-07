e4bc480ccfad393a27cb88784c4c57d0e1cc49cc
Ronak Mistry <ronakmistry36@gmail.com>
2022-06-07 18:08:30 -0500

    User agent configured

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,20 @@ import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { lookup } from 'geoip-lite';
 import { getClientIp } from 'request-ip';
+import { UAParser } from 'ua-parser-js';
 
 @Controller()
 export class AppController {
   @Get()
   appHandler(@Req() request: Request): Record<string, string | number | any> {
+    let responsePayload: Record<string, string | number | any> = {};
     const ipAddress = getClientIp(request);
     const enhancedData = lookup(ipAddress);
+    const userAgent = request.get('User-Agent') || null;
 
     if (enhancedData) {
-      return {
-        ipAddress,
+      responsePayload = {
+        ...responsePayload,
         country: enhancedData.country,
         region: enhancedData.region,
         city: enhancedData.city,
@@ -24,7 +27,19 @@ export class AppController {
       };
     }
 
+    if (userAgent) {
+      const uaParser = new UAParser();
+      uaParser.setUA(userAgent);
+      const enhancedUserAgent = uaParser.getResult();
+      responsePayload = {
+        ...responsePayload,
+        ...enhancedUserAgent,
+        userAgent,
+      };
+    }
+
     return {
+      ...responsePayload,
       ipAddress,
     };
   }
